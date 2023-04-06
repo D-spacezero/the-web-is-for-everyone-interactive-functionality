f03686a093875f193321ae6019aeb6defd6bde6c
D-spacezero <112856412+D-spacezero@users.noreply.github.com>
2023-04-06 10:14:35 +0200

    Local Host: Import

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import fs from "fs";
+const url = "https://api.ultitv.fdnd.nl/api/v1/players";
+
+// Maak een nieuwe express app
+const app = express();
+
+// Stel in hoe we express gebruiken
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+// Post request data zichtbaar
+app.use(express.urlencoded({ extended: false }));
+
+// Update speler binnen de json
+app.post("/players/:number", (request, response) => {
+  //Haal data uit de body van request
+  const { name, gender, nationality, team } = request.body;
+  // Speler nummer uit url
+  const plyNumber = request.params.number;
+  // haalt json uit filemap
+  var json = JSON.parse(fs.readFileSync("./public/data/players.json", "utf8"));
+  // zoekt index van speler in array
+  const index = json.players.findIndex(
+    (player) => player.number === Number(plyNumber)
+  );
+  //console.log(json.players[0]);
+
+  //Update speler met nieuwe data
+  json.players[index] = {
+    number: Number(plyNumber),
+    name: name,
+    gender: gender,
+    nationality: nationality,
+    team: team,
+  };
+
+  // Schrijft update terug naar file
+  fs.writeFileSync("./public/data/players.json", JSON.stringify(json, null, 2));
+  // Redirect home page
+  response.redirect("/");
+});
+
+// Maak een route voor de index
+app.get("/", (request, response) => {
+  response.render("index");
+});
+
+// Stel het poortnummer in en start express
+app.set("port", process.env.PORT || 8100);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
---
+++ public/scripts/script2.js
@@ -0,0 +1,206 @@
+// select the <ul> elements from the HTML document
+const playerList = document.querySelector("#player-list");
+const playerNumberList = document.querySelector("#player-number-list");
+const playerNameList = document.querySelector("#player-name-list");
+const playerGenderList = document.querySelector("#player-gender-list");
+const playerInfoList = document.querySelector("#player-info-list");
+
+// read the player data from the JSON file
+fetch("./data/players.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // access the player array
+    const players = data.players;
+
+    const overlayContent = document.querySelector("#overlay-content");
+
+    const form = document.createElement("form");
+    form.method = "POST";
+    form.style.display = "none";
+    form.id = "updateForm";
+
+    overlayContent.appendChild(form);
+
+    const playerDiv = document.createElement("div");
+    playerDiv.id = "playerDiv";
+    overlayContent.appendChild(playerDiv);
+
+    const editButton = document.createElement("button");
+
+    editButton.innerText = "edit";
+
+    editButton.addEventListener("click", () => {
+      // wissel naar form
+      if (form.style.display === "none") {
+        form.style.display = "block";
+        playerDiv.style.display = "none";
+      } else {
+        form.style.display = "none";
+        playerDiv.style.display = "block";
+      }
+    });
+
+    overlayContent.appendChild(editButton);
+
+    // loop through the players array and create a <li> element for each player
+    for (let i = 0; i < players.length; i++) {
+      // create a new <li> element for player name
+      const playerNameListItem = document.createElement("li");
+      //playerNameListItem.classList.add('ply-name');
+      playerNameListItem.textContent = players[i].name;
+
+      // create a new <li> element for player number
+      const playerNumberListItem = document.createElement("li");
+      playerNumberListItem.textContent = players[i].number;
+
+      // create a new <li> element for player gender
+      const playerGenderListItem = document.createElement("li");
+      playerGenderListItem.textContent = players[i].gender;
+
+      // create a new <button> element for the player information button
+      const infoButton = document.createElement("button");
+
+      // create a new <img> element for the SVG icon
+      const icon = document.createElement("img");
+      icon.setAttribute("src", "./media/info.svg");
+      icon.setAttribute("alt", "Player information");
+
+      // add the SVG icon to the button
+      infoButton.appendChild(icon);
+
+      // add an event listener to the button to handle the click event
+      infoButton.addEventListener("click", () => {
+        // get the player data for the current index from the players array
+        const currentPlayer = players[i];
+
+        form.action = `/players/${currentPlayer.number}`;
+
+        // display the player data in an overlay
+        const overlay = document.querySelector("#overlay");
+        const closeButton = document.querySelector("#close-button");
+
+        overlay.style.display = "block";
+
+        const playerTitle = document.createElement("h2");
+        playerTitle.innerText = `Name: ${currentPlayer.name}`;
+
+        const playerNumber = document.createElement("p");
+        playerNumber.innerText = `Number: ${currentPlayer.number}`;
+
+        const playerGender = document.createElement("p");
+        playerGender.innerText = `Gender: ${currentPlayer.gender}`;
+
+        const playerNationality = document.createElement("p");
+        playerNationality.innerText = `Nationality: ${currentPlayer.nationality}`;
+
+        const playerTeam = document.createElement("p");
+        playerTeam.innerText = `Team: ${currentPlayer.team}`;
+
+        playerDiv.appendChild(playerTitle);
+        playerDiv.appendChild(playerNumber);
+        playerDiv.appendChild(playerGender);
+        playerDiv.appendChild(playerNationality);
+        playerDiv.appendChild(playerTeam);
+
+        // make an input for each field
+
+        const formTitle = document.createElement("h2");
+        formTitle.innerText = `Name: ${currentPlayer.name}`;
+
+        form.appendChild(formTitle);
+
+        const nameFormDiv = document.createElement("div");
+        const nameLabel = document.createElement("label");
+        nameLabel.htmlFor = "name";
+        nameLabel.innerText = "Naam: ";
+
+        const nameInput = document.createElement("input");
+        nameInput.name = "name";
+        nameInput.value = currentPlayer.name;
+
+        nameFormDiv.appendChild(nameLabel);
+        nameFormDiv.appendChild(nameInput);
+
+        const numberFormDiv = document.createElement("div");
+        const numberLabel = document.createElement("label");
+        numberLabel.htmlFor = "number";
+        numberLabel.innerText = "Number: ";
+
+        const numberInput = document.createElement("input");
+        numberInput.name = "number";
+        numberInput.value = currentPlayer.number;
+
+        numberFormDiv.appendChild(numberLabel);
+        numberFormDiv.appendChild(numberInput);
+
+        const genderFormDiv = document.createElement("div");
+        const genderLabel = document.createElement("label");
+        genderLabel.htmlFor = "gender";
+        genderLabel.innerText = "Gender: ";
+
+        const genderInput = document.createElement("input");
+        genderInput.name = "gender";
+        genderInput.value = currentPlayer.gender;
+
+        genderFormDiv.appendChild(genderLabel);
+        genderFormDiv.appendChild(genderInput);
+
+        const natFormDiv = document.createElement("div");
+        const natLabel = document.createElement("label");
+        natLabel.htmlFor = "nationality";
+        natLabel.innerText = "Nationality: ";
+
+        const natInput = document.createElement("input");
+        natInput.name = "nationality";
+        natInput.value = currentPlayer.nationality;
+
+        natFormDiv.appendChild(natLabel);
+        natFormDiv.appendChild(natInput);
+
+        const teamFormDiv = document.createElement("div");
+        const teamLabel = document.createElement("label");
+        teamLabel.htmlFor = "team";
+        teamLabel.innerText = "Team: ";
+
+        const teamInput = document.createElement("input");
+        teamInput.name = "team";
+        teamInput.value = currentPlayer.team;
+
+        teamFormDiv.appendChild(teamLabel);
+        teamFormDiv.appendChild(teamInput);
+
+        form.appendChild(nameFormDiv);
+        form.appendChild(numberFormDiv);
+        form.appendChild(genderFormDiv);
+        form.appendChild(natFormDiv);
+        form.appendChild(teamFormDiv);
+
+        const saveButton = document.createElement("button");
+        saveButton.type = "submit";
+        saveButton.innerText = "save";
+
+        form.appendChild(saveButton);
+
+        // add an event listener to the close button to hide the overlay when clicked
+        closeButton.addEventListener("click", () => {
+          form.style.display = "none";
+          playerDiv.style.display = "block";
+          form.innerHTML = "";
+          playerDiv.innerHTML = "";
+
+          overlay.style.display = "none";
+        });
+      });
+
+      // add the <li> elements to the corresponding <ul> elements
+      playerNameList.appendChild(playerNameListItem);
+      playerNumberList.appendChild(playerNumberListItem);
+      playerGenderList.appendChild(playerGenderListItem);
+
+      // add the player information button to the <li> element for player name
+      playerInfoList.appendChild(infoButton);
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
